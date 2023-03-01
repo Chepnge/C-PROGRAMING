1fc58e7bf65cb4e2e01ada023c713a30e3767ff2
Chepnge <123072339+Chepnge@users.noreply.github.com>
2023-03-01 13:31:12 +0300

    Add files via upload

---
+++ volume.c
@@ -0,0 +1,16 @@
+//program to find volume of cylinder
+#include <stdio.h>
+int main()
+{ 
+    double r, h, v; 
+    printf("Enter radius and height: "); 
+    scanf("%lf %lf", &r, &h);   
+
+    // Calculating volume 
+    v = 3.14 * r * r * h; 
+
+    // %.2lf displays number up to 2 decimal point 
+    printf("Volume = %.2lf", v); 
+
+    return 0; 
+}
